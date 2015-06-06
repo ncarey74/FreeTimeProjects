74f68b614990bce698b97127a0d75c5babef9f4e
Carey Norslien <careynorslien@gmail.com>
2015-06-06 01:37:56 -0500

    SMaP Initial Check In
    The initial check in for the Simple Math Program. It'll essentially end
    up being a command line calculator. The fault reporting for this
    iteration isn't working properly.

---
+++ SMaP_Main.cpp
@@ -0,0 +1,241 @@
+//###########################################################################
+// Simple Math Program Main
+// FILE DESC HERE
+// 
+// Software Interface Specifications:
+//    IN:         N/A
+//    OUT:        N/A
+//
+// Author:        Carey Norslien
+// Created:       06/05/2015
+// Last Modified: 06/05/2015
+//###########################################################################
+
+//**FILE INCLUDES************************************************************
+#include    <iostream>
+#include    <string>
+#include    <climits>
+#include    <cerrno>
+
+//**FILE CONSTANTS***********************************************************
+const int   CMD_LENGTH          = 4u;
+
+const int   MAX_INT             = LONG_MAX; //"redefine" macro for Visual C++ limits
+const int   MIN_INT             = LONG_MIN; //"redefine" macro for Visual C++ limits
+
+const int   SAFE_DATA           = 0;        //when used in conjuction with another condition,
+                                            //  it can replace erroneous data
+
+//**CUSTOM TYPES*************************************************************
+enum Errors
+{
+    e_ERR_NONE = 0,
+    e_ERR_PARSE_CMD,
+    e_ERR_DATA_RANGE,
+};
+
+//**FILE DATA****************************************************************
+struct t_userInput
+{
+    int number1;
+    int number2;
+} g_userData;
+
+struct t_argumentErrors
+{
+    bool argumentCountHi    = false;
+    bool argumentCountLo    = false;
+} g_argumentErrors;
+
+//TODO: make a "standard" struct and an "enchanced" struct
+struct t_dataBoundErrors 
+{
+    bool dataRangeHi        = false;    //exceeds VC++ int upper limit
+    long dataRangeHiVal     = 0;
+    bool dataRangeLo        = false;    //exceeds VC++ int lower limit
+    long dataRangeLoVal     = 0;
+    bool dataRange          = false;    //exceeds 64 bits
+} g_operandsDataBoundError, g_answerDataBoundError;
+
+
+//**FUNCTION DECLARATIONS****************************************************
+//---------------------------------------------------------------------------
+// FUNC NAME
+// FUNC DESC
+// param:   N/A
+// return:  N/A
+//---------------------------------------------------------------------------
+void printErrorReport();
+
+//---------------------------------------------------------------------------
+// Parse Data
+// FUNC DESC
+//
+// Known Deficiency:
+//      All operands share the same fault reporting mechanism. 
+//      As such, a single fault may be triggered by more than one operand, so
+//      the latest operand with that fault will be reported without mention of
+//      previous operands with that fault.
+//
+// param:   (IN) operand    ...
+//          (OUT) error     ...
+// return:  Integer value of operand if it can fit in a 32 bit number, SAFE_DATA
+//          if the operand can't fit. SAFE_DATA must be used in conjunction with
+//          e_ERR_DATA_RANGE to properly handle erroneous data.
+//---------------------------------------------------------------------------
+int parseData(const char* operand, int& error);
+
+//---------------------------------------------------------------------------
+// FUNC NAME
+// FUNC DESC
+// param:   N/A
+// return:  N/A
+//---------------------------------------------------------------------------
+int parseCommands(int argc, char* argv[]);
+
+//---------------------------------------------------------------------------
+// FUNC NAME
+// FUNC DESC
+// param:   N/A
+// return:  N/A
+//---------------------------------------------------------------------------
+bool addition(int num1, int num2);
+
+
+//**FUNCTION DEFINITIONS*****************************************************
+
+void main(int argc, char* argv[])
+{
+    if (parseCommands(argc, argv) == e_ERR_NONE)
+    {
+        addition(g_userData.number1, g_userData.number2);
+    }
+    else
+    {
+        printErrorReport();
+    }
+}
+
+void printErrorReport()
+{
+    if (g_argumentErrors.argumentCountHi)
+    {
+        std::cout << "\nThere are too many arguments\n";
+    }
+    if (g_argumentErrors.argumentCountLo)
+    {
+        std::cout << "\nThere are too few arguments\n";
+    }
+    if (g_operandsDataBoundError.dataRangeHi)
+    {
+        std::cout << "\nNumber: " << g_operandsDataBoundError.dataRangeHiVal
+            << "is too large.\n";
+    }
+    if (g_operandsDataBoundError.dataRangeLo)
+    {
+        std::cout << "\nNumber: " << g_operandsDataBoundError.dataRangeLoVal
+            << "is too small.\n";
+    }
+    if (g_answerDataBoundError.dataRangeHi)
+    {
+        std::cout << "\nSum is too large.\n";
+    }
+    if (g_answerDataBoundError.dataRangeLo)
+    {
+        std::cout << "\nSum is too small.\n";
+    }
+    if (g_operandsDataBoundError.dataRange)
+    {
+        perror("The following error has occured");
+    }
+}
+
+int parseCommands(int argc, char* argv[])
+{
+    int isReadSuccessful = e_ERR_PARSE_CMD;
+
+    if (argc < CMD_LENGTH)
+    {
+        g_argumentErrors.argumentCountLo = true;
+    }
+    else if (argc > CMD_LENGTH)
+    {
+        g_argumentErrors.argumentCountHi = true;
+    }
+    else
+    {
+        if (strcmp(argv[1], "-add") == 0)
+        {
+            g_userData.number1 = parseData(argv[2], isReadSuccessful);
+            g_userData.number2 = parseData(argv[3], isReadSuccessful);
+            if ((g_userData.number1 != SAFE_DATA && g_userData.number2 != SAFE_DATA) ||
+                 isReadSuccessful != e_ERR_DATA_RANGE)
+            {
+                isReadSuccessful = e_ERR_NONE;
+            }
+        }
+    }
+
+    return isReadSuccessful;
+}
+
+//WARNING: assumes arguments are integers
+//TODO: typechecking
+int parseData(const char* data, int& error)
+{
+    error = e_ERR_DATA_RANGE;
+    long operand = strtol(data, NULL, 10);
+
+    if (errno == ERANGE)
+    {
+        g_operandsDataBoundError.dataRange = true;
+    }
+    else if (operand > MAX_INT)
+    {
+        g_operandsDataBoundError.dataRangeHi = true;
+        g_operandsDataBoundError.dataRangeHiVal = operand;
+    }
+    else if (operand < MIN_INT)
+    {
+        
+        g_operandsDataBoundError.dataRangeLo = true;
+        g_operandsDataBoundError.dataRangeLoVal = operand;
+    }
+    else
+    {
+        error = e_ERR_NONE;
+    }
+
+    if (g_operandsDataBoundError.dataRange || g_operandsDataBoundError.dataRangeHi ||
+        g_operandsDataBoundError.dataRangeLo)
+    {
+        operand = SAFE_DATA;
+    }
+
+    return operand;
+}
+
+
+bool addition(int num1, int num2)
+{
+    bool isInRange = true;
+    int result = 0;
+
+    result = num1 + num2;
+
+    if ((num2 > 0) && (num1 > (MAX_INT - num2)))
+    {
+        g_answerDataBoundError.dataRangeHi = true;
+    }
+    else if ((num2 < 0) && (num1 < (MIN_INT - num2)))
+    {
+        g_answerDataBoundError.dataRangeLo = true;
+    }
+    else
+    {
+        std::cout << "\nSum: " << result << "\n";
+        isInRange = true;
+    }
+
+    return isInRange;
+}
